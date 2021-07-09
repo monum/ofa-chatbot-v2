a4e985090f3fe6c8e71c8760cd09531c1df71999
Michael Lawrence Evans <mlevans@users.noreply.github.com>
2021-07-09 11:23:06 -0400

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,24 @@
+import json
+import csv
+
+translations = []
+
+with open('spanish.csv', 'rt') as f:
+        reader = csv.DictReader(f, delimiter=',')
+
+        for row in reader:
+            translations.append(row['Spanish'])
+
+with open('flow.json', 'rt') as f:
+    data = json.load(f)
+
+    for i in range(len(data['states'])):
+        try:
+            data['states'][i]['properties']['body'] = translations[i]
+        except KeyError:
+            print('Nothing here!')
+            data['states'][i]['properties']['body'] = ''
+
+
+with open('flow-spanish.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
